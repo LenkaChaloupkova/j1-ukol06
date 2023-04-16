2290d57528720b09a6cb65bd1d4feb9de864c99f
mac <lenkschaloupkova@gmail.com>
2023-04-16 23:50:41 +0200

    Nedodělaný úkol 06

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -6,8 +6,19 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Aplikace extends JFrame {
+
+    private JTextField husyField;
+    private JTextField kraliciField;
+    private JTextField pocetHlavField;
+    private JTextField pocetNohouField;
+    private JLabel husyLabel;
+    private JLabel kraliciLabel;
+    private JLabel pocetHlavLabel;
+    private JLabel pocetNohouLabel;
+    private JButton vypocitatButton;
 
     public static void main(String[] args) {
         FlatLightLaf.setup();
@@ -28,11 +39,58 @@ public class Aplikace extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setIconImage(new ImageIcon(Aplikace.class.getResource("czechitas-icon.png")).getImage());
         setLayout(new MigLayout("wrap 2", "[right]rel[50:120:150,grow,fill]"));
-        setMinimumSize(new Dimension(250, 200));
+        setMinimumSize(new Dimension(300, 250));
 
-        //TODO implementovat formulář podle zadání
 
+        husyField = new JTextField();
+        husyLabel = new JLabel("Husy");
+        husyLabel.setDisplayedMnemonic('H');
+        husyLabel.setLabelFor(husyField);
+        add(husyLabel);
+        add(husyField);
+
+        kraliciField = new JTextField();
+        kraliciLabel = new JLabel("Králíci");
+        kraliciLabel.setDisplayedMnemonic('K');
+        kraliciLabel.setLabelFor(kraliciField);
+        add(kraliciLabel);
+        add(kraliciField);
+
+        pocetHlavField = new JTextField();
+        pocetHlavLabel = new JLabel("Počet hlav");
+        pocetHlavLabel.setDisplayedMnemonic('P');
+        pocetHlavLabel.setLabelFor(pocetHlavField);
+        add(pocetHlavLabel);
+        add(pocetHlavField);
+
+        pocetNohouField = new JTextField();
+        pocetNohouLabel = new JLabel("Počet nohou");
+        pocetNohouLabel.setDisplayedMnemonic('N');
+        pocetNohouLabel.setLabelFor(pocetNohouField);
+        add(pocetNohouLabel);
+        add(pocetNohouField);
         pack();
+
+        getRootPane().setDefaultButton(vypocitatButton);
+        vypocitatButton.addActionListener(this::handleVypocitat);
     }
 
+    private void handleVypocitat(ActionEvent actionEvent) {
+    getInputMethodListeners(husyField);
+    /*
+        String text1 = …;
+        int cislo1 = Integer.parseInt(text1);
+        int cislo2 = …;
+        String text2 = Integer.toString(cislo2);
+     */
+    }
+
+    private JPanel createButtonBar() {
+        vypocitatButton = new JButton("Vypočítat");
+        vypocitatButton.setMnemonic('V');
+
+        JPanel buttonBar = new JPanel(new MigLayout(null, "[middle, grow]"));
+        buttonBar.add(vypocitatButton);
+        return buttonBar;
+    }
 }
